727f4173781d23768f102631af3177a20492d64f
iimgera <iimgera28062004@gmail.com>
2023-05-17 11:30:33 +0600

    registered models in apps/users/admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
-from apps.users.models import User
+from django.contrib.auth.admin import UserAdmin
+from apps.users.models import User, Operator, Brigade, Client
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = (
-            'email', 'full_name', 'is_admin', )
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'role', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('role', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'password1', 'password2',
+                'role', 'is_staff', 'is_superuser'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
 
+
+@admin.register(Operator)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
     search_fields = ('full_name',)
+
+
+@admin.register(Brigade)
+class BrigadeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'members')
+    search_fields = ('name',)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'address', 'phone')
+    search_fields = ('company_name',)
