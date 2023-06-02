d6ecb8e6bc446643aa31541d50ae55248b98cb66
iimgera <iimgera28062004@gmail.com>
2023-06-03 00:11:29 +0600

    updated  apps/users/permissions.py

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -5,6 +5,7 @@ class OperatorPermission(BasePermission):
     def has_permission(self, request, view):
         if request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
             user = request.user
+
             if user.user_type == 'OPERATOR':
                 return True
             return False
